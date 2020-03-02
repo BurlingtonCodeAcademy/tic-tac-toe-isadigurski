9ff6459649d486ced593b4661012d122e92c3f3d
Ilya Sadigurski <isadigurski@gmail.com>
2020-03-02 14:22:15 -0500

    onlinetictactoe2

--- app.js
+++ app.js
@@ -3,7 +3,7 @@ const app = express()
 const path = require('path')
 const PORT = process.env.PORT || 5000
 
-app.use(express.static('/public'))
+app.use(express.static('public'))
 
 app.get('/', (request, response) => {
     response.sendFile(path.resolve('./public/index.html'))
