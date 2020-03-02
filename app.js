3f74f5a4cb6149f88309d5dd8b133ff8e0be22fa
Ilya Sadigurski <isadigurski@gmail.com>
2020-03-02 14:03:07 -0500

    livetictic

--- app.js
+++ app.js
@@ -14,4 +14,4 @@ app.get('*', (req, res) => {
     res.sendFile(path.resolve('./public/404.html'))
 })
 
-app.listen(PORT, () => {console.log('Running on port: ${PORT}')})
+app.listen(PORT, () => {console.log(`Running on port: ${PORT}`)})
